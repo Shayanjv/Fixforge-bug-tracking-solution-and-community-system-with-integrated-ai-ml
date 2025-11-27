f6f230985233a1805d1605d2d242360fac093c8e
Shayanjv <jvshayan1@gmail.com>
2025-11-28 01:17:04 +0530

    Remove node_modules and frontend files

--- fixforge backend/fixforge-backend/venv/test_gemini.py
+++
@@ -1,8 +0,0 @@
-import requests, os
-
-KEY = "AIzaSyACKbgPRKFTwD2tgFe6IRe3wyxsQ77Cfhs"
-URL = f"https://generativelanguage.googleapis.com/v1beta/models?key={KEY}"
-
-res = requests.get(URL, timeout=30)
-print(res.status_code)
-print(res.text)
